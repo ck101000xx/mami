29e300fabffefe81c8beee29f17866d0b29ad15b
ck101000xx <ck101000xx@gmail.com>
2015-06-24 20:54:56 +0800

    refactor

--- src/resource.hpp
+++ src/resource.hpp
@@ -1,22 +1,83 @@
-#ifndef RESOURCE_HPP
-#define RESOURCE_HPP
+#ifndef MAMI_RESOURCE_HPP
+#define MAMI_RESOURCE_HPP
 #include <atomic>
+#include <utility>
 extern "C" {
   #include <ppapi/c/pp_resource.h>
 }
+#include "resource_map.hpp"
+namespace mami {
+template<class T>
 class resource {
 public:
-    resource();
     virtual ~resource() = default;
-    void add_ref();
-    void release();
-    PP_Resource pp_resource();
-    friend void intrusive_ptr_add_ref(const resource*);
-    friend void intrusive_ptr_release(const resource*);
+    friend class resource_ptr<T>;
+protected:
+    resource() = default;
 private:
-    const PP_Resource key;
     std::atomic<int> ref_count = 0;
+};
+template<class T>
+class resource_ptr {
+private:
+    typedef resource_ptr<T> this_type;
+public:
+    template<typename ...Args>
+    static resource_ptr<T> & create(Args... args) {
+        return *new resource_ptr(new T(args...));
+    }
+    static void add_ref(PP_Resource key) {
+        this_type(key).add_ref();
+    }
+    static void release(PP_Resource key) {
+        this_type(key).release();
+    }
+    resource_ptr() : key(0), px(nullptr) {}
+    resource_ptr(PP_Resource key) : key(key) {
+        px = internal::resource_map::find<T>(key);
+        add_ref();
+    }
+    resource_ptr(this_type const & rhs) : key(rhs.key), px(rhs.px) {
+        add_ref();
+    }
+    resource_ptr(this_type && rhs) : key(rhs.key), px(rhs.px) {
+        rhs.px = nullptr;
+    }
+    this_type & operator=(this_tpye const & rhs) {
+        resource_ptr(rhs).swap(*this);
+        return *this;
+    }
+    this_type & operator=(this_type && rhs) {
+        resource_ptr(std::move(rhs)).swap(*this);
+        return *this;
+    }
+    ~resource_ptr() {
+        release();
+    }
+    T & operator*() const {
+        return *px;
+    }
+    T * operator->() const {
+        return px;
+    }
+private:
+    resourcr_ptr(resource * px) : px(px) {
+        key = internal::resource_map::insert<T>(res);
+        add_ref();
+    }
+    void add_ref() {
+        if (px != nullptr) ++px->ref_count;
+    }
+    void release() {
+        if (px != nullptr && --px->ref_count == 0) {
+            internal::resource_map::erase<T>(key);
+            delete px;
+        }
+    }
+    void swap(this_type & rhs) {
+        std::swap(key, rhs.key);
+        std::swap(px, rhs.px);
+    }
+};
 }
-void intrusive_ptr_add_ref(const resource*);
-void intrusive_ptr_release(const resource*);
 #endif
--- src/resource_map.hpp
+++ src/resource_map.hpp
@@ -1,24 +1,44 @@
 #ifndef RESOURCE_MAP_HPP
 #define RESOURCE_MAP_HPP
+#include <atomic>
 #include <utility>
-#include <boost/core/noncopyable.hpp>
-#include <boost/intrusive_ptr.hpp>
 #include <tbb/concurrent_hash_map.h>
-#include "resource.hpp"
 extern "C" {
   #include <ppapi/c/pp_resource.h>
 }
 
-class resource_map : private boost::noncopyable {
-public:
-    static resource_map& get_instance();
-    PP_Resource insert(resource *ptr);
-    boost::intrusive_ptr<resource>& find(PP_Resource key);
-    void erase(PP_Resource key);
+namespace mami {
+namespace internal {
+class resource_map {
 private:
-    resource_map() = default;
-    typedef tbb::concurrent_hash_map<PP_Resource, *resource> hash_map_t;
-    hash_map_t hash_map;
-    std::atomic<PP_Resource> last_resource_key = 0;
+    template<class T>
+    typedef tbb::concurrent_hash_map<PP_Resource, T *> hash_map_t;
+public:
+    template<class T>
+    static PP_Resource insert(T * p) {
+        PP_Resource key = next_key();
+        hash_map<T>.insert({key, p});
+        return key;
+    }
+    template<class T>
+    static T* find(PP_Resource key) {
+        hash_map_t<T>::const_accessor result;
+        return hash_map<T>.find(result, key) ? result->second : nullptr ;
+    }
+    template<class T>
+    static void erase(PP_Resourcre key) {
+        hash_map<T>.erase(key);
+    }
+private:
+    static PP_Resource next_key() {
+        static std::atomic<PP_Resource> last_key(0);
+        return ++last_key;
+    }
+    template<class T>
+    static const hash_map_t<T> hash_map;
+};
+template<class T>
+const resource_map::hash_map_t<T> resource_map::hash_map;
+}
 }
 #endif
